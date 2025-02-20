3b3b93953d9533ee9a5bc49dab3d4b3ef9c8a6e5
Daviss0 <davis.patricio1505@gmail.com>
2025-02-20 19:11:22 -0300

    subindo o projeto para o GitHub

---
+++ Ecommerce/src/main/java/com/PI4/Ecommerce/TechSellerApplication.java
@@ -0,0 +1,13 @@
+package com.PI4.Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSellerApplication.class, args);
+	}
+
+}
