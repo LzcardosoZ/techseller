39c0e3075772aec7f69080453ac22903132d7615
Luiz Felipe Santos <107314612+LzcardosoZ@users.noreply.github.com>
2025-02-20 23:46:03 -0300

    Delete Ecommerce directory

--- Ecommerce/src/main/java/com/PI4/Ecommerce/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seuusuario.ecommerce.repository;
-
-import com.seuusuario.ecommerce.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
