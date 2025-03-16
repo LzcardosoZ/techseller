b01624019262ec06640b0bf22d8e695dd9283f3b
Daviss0 <davis.patricio1505@gmail.com>
2025-03-16 00:14:43 -0300

    subindo o banco h2

--- src/main/java/br/com/techseller/techsellers/entity/User.java
+++ src/main/java/br/com/techseller/techsellers/entity/User.java
@@ -14,27 +14,38 @@ import jakarta.validation.constraints.Size;
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name="users", uniqueConstraints = { @UniqueConstraint(columnNames = "email") })
+@Table(name="USERS", uniqueConstraints = { @UniqueConstraint(columnNames = "email") })
 public class User {
     @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Geração automática do ID
+    @Column(name = "USER_ID")
     private Long user_id;
 
     @Getter
+    @Column(name = "USERNAME", nullable = false)
     private String username;
 
+    @Column(name = "CPF", unique = true, nullable = false)
     private String cpf;
 
+    @Column(name = "EMAIL", unique = true, nullable = false)
     @NotBlank(message = "O email é obrigatório")
     @Email(message = "Digite um email válido")
     private String email;
 
+    @Column(name = "GRUPO")
     private Grupo grupo;
+
+    @Column(name = "PASSWORD", nullable = false)
     @NotBlank(message = "A senha é obrigatória")
     private String password;
-    @Column(nullable = false)
+
+
+    @Column(name = "CONF_PASSWORD", nullable = false)
     private String confPassword;
+
+    @Column(name = "STATUS")
     @Getter
     private String status;
 
